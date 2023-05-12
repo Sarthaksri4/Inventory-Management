aa1c6d84b72910f988981f8cd43486c20dbc5a83
sarthak <nandini.srivastava.12.2004@gmail.com>
2023-05-12 08:02:52 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react'
+import { NavLink ,useNavigate} from "react-router-dom"
+import { ToastContainer, toast } from 'react-toastify';
+import Img1 from './Images/img1.png'
+import "./Login.css"
+import axios from "axios"
+
+const Login = () => {
+
+    // const [passShow, setPassShow] = useState(false);
+
+    // const [inpval, setInpval] = useState({
+    //     email: "",
+    //     password: "",
+    // });
+
+    // const history = useNavigate();
+
+    // const setVal = (e) => {
+    //     // console.log(e.target.value);
+    //     const { name, value } = e.target;
+
+    //     setInpval(() => {
+    //         return {
+    //             ...inpval,
+    //             [name]: value
+    //         }
+    //     })
+    // };
+
+
+    // const loginuser = (e) => {
+    //     e.preventDefault();
+
+        // const { email, password } = inpval;
+
+        // if (email === "") {
+        //     toast.error("email is required!", {
+        //         position: "top-center"
+        //     });
+        // } else if (!email.includes("@")) {
+        //     toast.warning("includes @ in your email!", {
+        //         position: "top-center"
+        //     });
+        // } else if (password === "") {
+        //     toast.error("password is required!", {
+        //         position: "top-center"
+        //     });
+        // } else if (password.length < 6) {
+        //     toast.error("password must be 6 char!", {
+        //         position: "top-center"
+        //     });
+        // } else {
+        //     console.log("user login succesfully done");
+        // }
+    
+    const [formdata,setformdata]=useState({ email:"", password:""});
+   function setVal(event){
+  // console.log(event.target.value);
+     setformdata(previousResponce=>{
+      return{
+      ...previousResponce,
+      [event.target.name]:event.target.value
+    }
+
+  });
+}
+const loginuser = (e) =>{
+    console.log(formdata);
+    axios.post("https://inventory-pyes.onrender.com/api/login",formdata)
+    .then((response)=>{
+     console.log(response);
+    })
+}
+
+
+           
+
+    return (
+        <>
+            <section>
+            <div className="form">
+                <div className="form_data">
+                     <img className='img1' src={Img1} />
+                    <div className="form_heading">
+                        <h1>Welcome!</h1>
+                        <p>Please,enter your details.</p>
+                    </div>
+
+                    <form>
+                        <div className="form_input">
+                            <label htmlFor="email">Email</label>
+                            <input type="email"  onChange={setVal} name="email" id="email" placeholder='Enter Your Email Address' />
+                        </div>
+                        <div className="form_input">
+                            <label htmlFor="password">Password</label>
+                            <div className="two">
+                                <input type= "password"  onChange={setVal}  name="password" id="password" placeholder='Enter Your password' />
+                                {/* <div className="showpass" onClick={() => setPassShow(!passShow)}>
+                                    {!passShow ? "Show" : "Hide"}
+                                </div> */}
+                            </div>
+                        </div>
+
+                        <button className='btn' onClick={loginuser}>Login</button>
+                        <p>Don't have an Account? <NavLink to="/register">Sign Up</NavLink> </p>
+                    </form>
+                    {/* <ToastContainer /> */}
+                </div>
+            </div>
+            </section>
+        </>
+    )
+}
+
+export default Login
---
+++ src/components/Register.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react'
+import { NavLink } from "react-router-dom"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Img1 from './Images/img1.png'
+import "./Register.css"
+import axios from "axios"
+
+const Register = () => {
+
+    // const [passShow, setPassShow] = useState(false);
+    // const [cpassShow, setCPassShow] = useState(false);
+
+    // const [inpval, setInpval] = useState({
+    //     fname: "",
+    //     email: "",
+    //     password: "",
+    //     cpassword: ""
+    // });
+
+
+    // const setVal = (e) => {
+    //     // console.log(e.target.value);
+    //     const { name, value } = e.target;
+
+    //     setInpval(() => {
+    //         return {
+    //             ...inpval,
+    //             [name]: value
+    //         }
+    //     })
+    // };
+
+    // const addUserdata = async (e) => {
+    //     e.preventDefault();
+
+    //     const { fname, email, password, cpassword } = inpval;
+
+    //     if (fname === "") {
+    //         toast.warning("fname is required!", {
+    //             position: "top-center"
+    //         });
+    //     } else if (email === "") {
+    //         toast.error("email is required!", {
+    //             position: "top-center"
+    //         });
+    //     } else if (!email.includes("@")) {
+    //         toast.warning("includes @ in your email!", {
+    //             position: "top-center"
+    //         });
+    //     } else if (password === "") {
+    //         toast.error("password is required!", {
+    //             position: "top-center"
+    //         });
+    //     } else if (password.length < 6) {
+    //         toast.error("password must be 6 char!", {
+    //             position: "top-center"
+    //         });
+    //     } else if (cpassword === "") {
+    //         toast.error("cpassword is required!", {
+    //             position: "top-center"
+    //         });
+    //     }
+    //     else if (cpassword.length < 6) {
+    //         toast.error("confirm password must be 6 char!", {
+    //             position: "top-center"
+    //         });
+    //     } else if (password !== cpassword) {
+    //         toast.error("pass and Cpass are not matching!", {
+    //             position: "top-center"
+    //         });
+    //     } else {
+            // console.log("user registration succesfully done");
+
+
+            // const data = await fetch("/register", {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json"
+            //     },
+            //     body: JSON.stringify({
+            //         fname, email, password, cpassword
+            //     })
+            // });
+
+            // const res = await data.json();
+            // // console.log(res.status);
+
+            // if (res.status === 201) {
+            //     toast.success("Registration Successfully done 😃!", {
+            //         position: "top-center"
+            //     });
+            //     setInpval({ ...inpval, fname: "", email: "", password: "", cpassword: "" });
+            // }
+        // }
+    // }
+
+    const [formdata,setformdata]=useState({ username:"", email:"", password:"", confirmpassword:""});
+    function setVal(event){
+   // console.log(event.target.value);
+      setformdata(previousResponce=>{
+       return{
+       ...previousResponce,
+       [event.target.name]:event.target.value
+     }
+ 
+   });
+ }
+ const  addUserdata = (e) =>{
+     console.log(formdata);
+     axios.post("https://inventory-pyes.onrender.com/api/register",formdata)
+     .then((response)=>{
+      console.log(response);
+     })
+ }
+ 
+ return (
+        <>
+            <section>
+            <div className="form">
+                <div className="forms_data">
+                     <img className='img1' src={Img1} />
+                    <div className="forms_heading">
+                        <h1>Welcome!</h1>
+                        <p style={{ textAlign: "center" }}>Please,enter your details</p>
+                    </div>
+
+                    <form>
+                        <div className="forms_input">
+                            <label htmlFor="fname">Name</label>
+                            <input type="text" onChange={setVal}  name="name" id="name" placeholder='Enter Your Name' />
+                        </div>
+                        <div className="forms_input">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" onChange={setVal}  name="email" id="email" placeholder='Enter Your Email Address' />
+                        </div>
+                        <div className="forms_input">
+                            <label htmlFor="password">Password</label>
+                            <div className="two">
+                                <input type="password" onChange={setVal} name="password" id="password" placeholder='Enter Your password' />
+                            </div>
+                        </div>
+
+                        <div className="forms_input">
+                            <label htmlFor="password">Confirm Password</label>
+                            <div className="two">
+                                <input type="password"  onChange={setVal} name="confirmpassword" id="confirmpassword" placeholder='Confirm password' />
+                            </div>
+                        </div>
+
+                        <button className='btn' onClick={addUserdata}>Sign Up</button>
+                        <p>Already have an account? <NavLink to="/">Log In</NavLink></p>
+                    </form>
+                    {/* <ToastContainer /> */}
+                </div>
+            </div>
+            </section>
+        </>
+    )
+}
+
+export default Register
